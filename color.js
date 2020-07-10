ab1ede5650f292db73018bcc0614566329ef7650
H43RO <h2is1234@naver.com>
2020-07-11 01:57:18 +0900

    jQuery

--- color.js
+++ color.js
@@ -1,20 +1,23 @@
 var Links = {
     setColor: function(color) {
-        var alist = document.querySelectorAll('a');
-        var i = 0;
-        while (i < alist.length) {
-            alist[i].style.color = color;
-            i++;
-        }
+        // var alist = document.querySelectorAll('a');
+        // var i = 0;
+        // while (i < alist.length) {
+        //     alist[i].style.color = color;
+        //     i++;
+        // }
+        $('a').css('color', color); //jQuery Function
     }
 }
 
 var Body = {
     setColor: function(color) {
-        document.querySelector('body').style.color = color;
+        // document.querySelector('body').style.color = color;
+        $('body').css('color', color);
     },
     setBackgroundColor: function(color) {
-        document.querySelector('body').style.backgroundColor = color;
+        // document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
     }
 }
 
